4116a88f6f4df106a952e47e7b1b17a63787ea93
DavidLu1997 <david.lu@hotmail.com>
2016-09-16 19:24:12 -0400

    added function prototypes

---
+++ api/report.js
@@ -0,0 +1,11 @@
+modules.exports = {
+    get: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    update: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    submit: function(httpRequest, httpResponse) {
+        // TODO
+    }
+}

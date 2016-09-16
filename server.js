26232e296624566a2ae4da5718be73a130706dd1
Denton Liu <liu.denton+github@gmail.com>
2016-09-16 17:57:18 -0400

    Create other routes

--- server.js
+++ server.js
@@ -20,15 +20,56 @@ userRouter.route('/logout').post(function(httpRequest, httpResponse) {
     // TODO
 });
 
-userRouter.route('/delete').delete(function(httpRequest, httpResponse) {
+userRouter.route('/delete').post(function(httpRequest, httpResponse) {
     // TODO
 });
 
 const floorPlanRouter = express.Router({mergeParams: true});
+floorPlanRouter.route('/add').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+floorPlanRouter.route('/delete').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+floorPlanRouter.route('/list').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+floorPlanRouter.route('/show').post(function(httpRequest, httpResponse) {
+    // TODO
+});
 
 const roomRouter = express.Router({mergeParams: true});
+roomRouter.route('/generate').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+roomRouter.route('/list').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+roomRouter.route('/:id/get').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+roomRouter.route('/:id/delete').post(function(httpRequest, httpResponse) {
+    // TODO
+});
 
 const printoutRouter = express.Router({mergeParams: true});
+printoutRouter.route('/room/:id').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+printoutRouter.route('/report').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+printoutRouter.route('/report/:id').post(function(httpRequest, httpResponse) {
+    // TODO
+});
 
 const analyticsRouter = express.Router({mergeParams: true});
 // TODO
@@ -40,6 +81,17 @@ landlordRouter.use('/printout', printoutRouter);
 landlordRouter.use('/analytics', analyticsRouter);
 
 const reportRouter = express.Router({mergeParams: true});
+reportRouter.route('/').get(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+reportRouter.route('/update').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+reportRouter.route('/submit').post(function(httpRequest, httpResponse) {
+    // TODO
+});
 
 const apiRouter = express.Router();
 apiRouter.use('/user', userRouter);

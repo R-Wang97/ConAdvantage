78fdfa4385710d7da81ff9d63365e980b535bb09
DavidLu1997 <david.lu@hotmail.com>
2016-09-16 17:40:36 -0400

    added basic framework

--- server.js
+++ server.js
@@ -7,6 +7,46 @@ const config = JSON.parse(
     fs.readFileSync(configFile)
 );
 
+const userRouter = express.Router({mergeParams: true});
+userRouter.route('/create').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+userRouter.route('/login').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+userRouter.route('/logout').post(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+userRouter.route('/delete').delete(function(httpRequest, httpResponse) {
+    // TODO
+});
+
+const floorPlanRouter = express.Router({mergeParams: true});
+
+const roomRouter = express.Router({mergeParams: true});
+
+const printoutRouter = express.Router({mergeParams: true});
+
+const analyticsRouter = express.Router({mergeParams: true});
+// TODO
+
+const landlordRouter = express.Router({mergeParams: true});
+landlordRouter.use('/floorplan', floorPlanRouter);
+landlordRouter.use('/room', roomRouter);
+landlordRouter.use('/printout', printoutRouter);
+landlordRouter.use('/analytics', analyticsRouter);
+
+const reportRouter = express.Router({mergeParams: true});
+
+const apiRouter = express.Router();
+apiRouter.use('/user', userRouter);
+apiRouter.use('/landlord', landlordRouter);
+apiRouter.use('/:id', reportRouter);
+
+app.use('/api', apiRouter);
 app.use('/', express.static(`${__dirname}/public`));
 
 const port = config.ServerPort;

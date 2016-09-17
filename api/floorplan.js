2d5e0c28e0963cffb8cf4d1359f4be7a2db18faf
David Lu <david.lu97@outlook.com>
2016-09-17 00:37:11 -0400

    Server framework (#3)
    * added basic framework
    
    * Create other routes
    
    * added api directory
    
    * added function prototypes
    
    * fixed syntax

---
+++ api/floorplan.js
@@ -0,0 +1,14 @@
+module.exports = {
+    add: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    delete: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    list: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    show: function(httpRequest, httpResponse) {
+        // TODO
+    }
+}
---
+++ api/printout.js
@@ -0,0 +1,11 @@
+module.exports = {
+    generate: function(httpRequest, httpReponse) {
+        // TODO
+    },
+    reportAll: function(httpRequest, httpResponse) {
+        // TODO
+    },
+    report: function(httpRequest, httpReponse) {
+        // TODO
+    }
+}

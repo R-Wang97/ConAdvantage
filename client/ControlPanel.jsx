42f3ab8855562bd45b531babb9a9166116f3de82
Ryan Wang <shirui.wang@hotmail.com>
2016-09-16 21:30:21 -0400

    Created all React Component Templates

---
+++ client/ControlPanel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ControlPanel extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	onSubmit = () => {
+		//Endpoint is "/api/{id}/submit"
+	}
+
+	render() {
+		return (
+			<div/>
+		);
+	}
+}
+
+export default ControlPanel
---
+++ client/FloorPlanImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class FloorPlanImage extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<img></img>
+		);
+	}
+}
+
+export default FloorPlanImage

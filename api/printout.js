bd4454fe35a4dab36752c392d46e9d8c000e3a63
Denton Liu <liu.denton+github@gmail.com>
2016-09-17 01:08:41 -0400

    Correct David's typo

--- api/printout.js
+++ api/printout.js
@@ -1,14 +1,15 @@
 'use strict';
 const db = require('./database.js');
+const pdfMake = require('pdfmake');
 
 module.exports = {
-    produce: function(httpRequest, httpReponse) {
+    produce: function(httpRequest, httpResponse) {
         // TODO
     },
     reportAll: function(httpRequest, httpResponse) {
         // TODO
     },
-    report: function(httpRequest, httpReponse) {
+    report: function(httpRequest, httpResponse) {
         // TODO
     }
 }

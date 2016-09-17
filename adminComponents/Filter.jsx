76cbd6bb4a18437b557d2e050194ee0d1aa80b5b
allenhuang97 <allen.weixi.huang@gmail.com>
2016-09-17 17:18:49 -0400

    Working on Admin CSS

--- adminComponents/Filter.jsx
+++ adminComponents/Filter.jsx
@@ -8,27 +8,41 @@ class Filter extends React.Component {
 	render() {
 		return(
 			<div className='container'>
-				<div className='row' id='filter1'>
-					<label className='col-md-4' id='itemSelect'>
-						<select>
-							<option value='item1'>Item1</option>
-							<option value='item2'>Item2</option>
-							<option value='item3'>Item3</option>
-							<option value='item4'>Item4</option>
-						</select>
-					</label>
-					<label className='col-md-4' id='conditionSelect'>
-						<select>
-							<option value='good'>Good</option>
-							<option value='missing'>Missing</option>
-							<option value='damaged'>Damaged</option>							
-						</select>
-					</label>
-				</div>
-				<div className='row' id='buttonBar'>
-					{/* Add onClick for these two buttons. */}
-					<button type='button' className='btn btn-primary btn-md'>Search</button>
-                    <button type='button' className='btn btn-default btn-md'>Clear</button>
+				<div id='filterContent'>
+					<div className='row' id='filterText'>
+						<h1>Filter Settings</h1>
+						<hr />
+					</div>
+					<div className='row' id='filter1'>
+						<label className='col-md-4' id='buildingSelect'>
+							Building:
+							<select>
+								<option value='T1'>T1</option>
+								<option value='T2'>T2</option>							
+							</select>
+						</label>
+						<label className='col-md-4' id='itemSelect'>
+							Item:
+							<select>
+								<option value='item1'>Item1</option>
+								<option value='item2'>Item2</option>
+								<option value='item3'>Item3</option>
+								<option value='item4'>Item4</option>
+							</select> 
+						</label>
+						<label className='col-md-4' id='conditionSelect'>
+							Condition:
+							<select>
+								<option value='good'>Good</option>
+								<option value='missing'>Missing</option>
+								<option value='damaged'>Damaged</option>							
+							</select>
+						</label>
+					</div>
+					<div className='row' id='btnFilter'>
+						{/* Add onClick for button */}
+						<button type='button' className='btn btn-primary btn-block'>Search</button>
+					</div>
 				</div>
 			</div>
 		);

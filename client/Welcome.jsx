564efd3c74b7f57e5df9e971dac80153afe3afb9
allenhuang97 <allen.weixi.huang@gmail.com>
2016-09-16 21:35:31 -0400

    Working on Welcome Page CSS

--- client/Welcome.jsx
+++ client/Welcome.jsx
@@ -13,20 +13,25 @@ class Welcome extends React.Component {
 	render() {
 		const welcomeScreenContent = (
 			<div className = 'container'>
-				<div className = 'row' id = 'pageName'>
-					<h1>Room Conditions Form</h1>
-				</div>
-				<div className = 'row' id = 'tenantInfo'>
-					<h4>Hello {this.state.name}</h4>
-					<h4>Your Room Number is: {this.state.roomNum}</h4>
-					<h4>Your Room Type: {this.state.roomType}</h4>
-				</div>
-				<div className = 'row' id = 'confirmInfo'>
-					<h4>If this information is correct please proceed to the next page and complete the room conditions form.</h4>
-					<h4>If this is not you please contact ~~~~~~~~~~</h4> 
-				</div>
-				<div className = 'row'>
-					<button type = 'button' className = 'btn btn-primary btn-lg btn-block'>Continue to Form >></button>
+				<div className = 'contents'>
+					<div className = 'center' id = 'welcomePageName'>
+						<h1>Room Condition Form</h1>
+						<hr />
+					</div>
+					<div className = 'row' id = 'welcomeTenantInfo'>
+						<div className = 'col-md-4'>
+							<h3>Hello (name){this.state.name}</h3>
+							<h3>Your Room Number is: (room #){this.state.roomNum}</h3>
+							<h3>Your Room Type: (room type){this.state.roomType}</h3>
+						</div>
+					</div>
+					<div className = 'row' id = 'welcomeConfirmInfo'>
+						<h5>If this information is correct please proceed to the next page and complete the room conditions form.</h5>
+						<h5>If this is not you please contact ~~~~~~~~~~</h5> 
+					</div>
+					<div className = 'center' id = 'welcomeButton'>
+						<button type = 'button' className = 'btn btn-primary btn-lg btn-block'><h3>Continue to Form >></h3></button>
+					</div>
 				</div>
 			</div>
 		);
